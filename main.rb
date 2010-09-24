9627e870a61b055ff1d61f35bacd219097c84dee
vovka <sch.icq@gmail.com>
2010-09-24 15:30:37 +0300

    colorized

--- main.rb
+++ main.rb
@@ -22,15 +22,15 @@ else
 end
 
 
-c = 100
+c = :q
 b.val = c
-c = 200
+c = :w
 
 puts "-> c.class # => #{c.class}\n-> b.val = c"
-if b.val == 200
+if b.val == :w
   puts "Поле 'val' объекта 'b' является ссылкой на объект 'c' класса '#{b.val.class}'"
 else
   puts "Поле 'val' объекта 'b' является копией объекта 'c' класса '#{b.val.class}'"
 end
 
-puts "В чём разница между объектами класса '#{a.b.class}' и класса '#{b.val.class}'?"
+puts "\n\e[1;33;40mВ чём разница между объектами класса '#{a.b.class}' и класса '#{b.val.class}'?\e[0m\n\n"
